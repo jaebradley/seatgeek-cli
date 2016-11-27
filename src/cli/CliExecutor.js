9f57be7a5830ef980de19d7030e6104dfbab534c
jaebradley <jae.b.bradley@gmail.com>
2016-11-26 23:28:15 -0500

    add other options

--- src/cli/CliExecutor.js
+++ src/cli/CliExecutor.js
@@ -1,6 +1,7 @@
 'use es6';
 
 import program from 'commander';
+import moment from 'moment';
 
 import EventsSearch from '../data/EventsSearch';
 import EventDetailsTableBuilder from '../tables/builders/EventDetailsTableBuilder';
@@ -9,19 +10,46 @@ export default class CliExecutor {
   static execute() {
     program.version('0.0.1');
 
-    program.command('events [city] [state]')
-           .action((city, state) => CliExecutor.action(city, state));
+    program.option('-c', '--city [city]')
+           .option('-s', '--state [state]')
+           .option('-d', '--datetime [datetime]')
+           .option('-t', '--type [type]')
+           .action(function() {
+             CliExecutor.action(CliExecutor.parseRawArgs(program.rawArgs));
+           });
 
     program.parse(process.argv);
 
   }
 
-  static action(city, state) {
-    let search = new EventsSearch({
-      cityName: city,
-      stateCode: state,
+  static parseRawArgs(args) {
+    let cityName = undefined;
+    let stateCode = undefined;
+    let datetime = moment.utc()
+                    .add(3, 'months')
+                    .format('YYYY-MM-DDTHH:mm:ss');
+    let type = undefined;
+    for (let i = 0; i < args.length; i++) {
+      let arg = args[i];
+      let nextArg = args[i + 1];
+      if ((arg == '-c') || (arg == '--city')) {
+        cityName = nextArg;
+      } else if ((arg == '-s') || (arg == '--state')) {
+        stateCode = nextArg;
+      } else if ((arg == '-d') || (arg == '--datetime')) {
+        datetime = nextArg;
+      } else if ((arg == '-t') || (arg == '--type')) {
+        type = nextArg;
+      }
+    }
+    return new EventsSearch({
+      cityName: cityName,
+      stateCode: stateCode,
+      datetime: datetime,
     });
+  }
 
+  static action(search) {
     EventDetailsTableBuilder.buildTable(search);
   }
 }
