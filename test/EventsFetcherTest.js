b3bb21639e5c74934da0bcecab89568c183eb49a
jaebradley <jae.b.bradley@gmail.com>
2016-12-18 21:15:27 -0500

    update

--- test/EventsFetcherTest.js
+++ test/EventsFetcherTest.js
@@ -6,12 +6,13 @@ import EventsFetcher from '../src/fetcher/EventsFetcher';
 import EventsSearch from '../src/data/EventsSearch';
 
 describe('Test EventsFetcher', function() {
+  let fetcher = new EventsFetcher();
   it('test event fetching', function() {
     let search = new EventsSearch({
       cityName: 'boston',
       stateCode: 'ma',
     });
-    return EventsFetcher.fetchEventsByPopularityOrderedByLowestPriceGoodDeals(search)
-                        .then(events => console.log(events));
+    return fetcher.fetchEventsByPopularityOrderedByLowestPriceGoodDeals(search)
+                  .then(events => console.log(events));
   });
 });
